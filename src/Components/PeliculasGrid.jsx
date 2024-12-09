af04d1bd1f3cc107798c0a1632e8ba030ae66812
Natali Schwartz <schwartznatali@gmail.com>
2024-12-09 14:54:51 +0100

    configure the API credentials

--- src/Components/PeliculasGrid.jsx
+++ src/Components/PeliculasGrid.jsx
@@ -1,6 +1,6 @@
 /*Peliculas grid el componente , va a consumir el json , lo va a mapear, no lo vamos a pintar aca, y despues se lo voy a pasar como parametro como prop a peliculas card el li va a ser el componente PeliculasCard. pelicula es la variable donde voy a ir guardando cada pelicula del recorrido.*/
 
-import peliculas from "./peliculas.json"
+import { get } from "../utils/httpCliente"
 import { PeliculasCard } from "./PeliculasCard"
 import "./PeliculasGrid.css"
 
